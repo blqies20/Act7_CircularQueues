8ef7524c533f8fd2b466fc3ddec3f550d2e51292
ACER <ACER@DESKTOP-QH5VR0V>
2022-12-14 17:22:06 +0700

    add class public void display

--- Act7_CircularQueues/Program.cs
+++ Act7_CircularQueues/Program.cs
@@ -61,5 +61,45 @@ namespace CircularQueues_CSharp
                     FRONT = FRONT + 1;
             }
         }
+        public void display()
+        {
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+            /*checks if the queue is empty*/
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+            Console.WriteLine("\nElements in the queue are ............. \n");
+            if (FRONT_position <= REAR_position)
+            {
+                /* traverse the queue till the last element present in a array. */
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "   ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                /**/
+                while (FRONT_position <= max - 1)
+                {
+                    Console.Write(queue_array[FRONT_position] + "   ");
+                    FRONT_position++;
+                }
+                /* set the FRONT position to the first element of the array. */
+                FRONT_position = 0;
+                /* traverses the array till the last element present in the queue */
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "   ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
